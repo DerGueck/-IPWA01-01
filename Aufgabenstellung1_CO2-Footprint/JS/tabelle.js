875a1f93af8abf86157b319d9d5a36ac209da1c3
DerGueck <dergueck@googlemail.com>
2025-03-02 15:33:08 +0100

    FInalFinal Abgabe erfolgt

--- Aufgabenstellung1_CO2-Footprint/JS/tabelle.js
+++ Aufgabenstellung1_CO2-Footprint/JS/tabelle.js
@@ -1,9 +1,3 @@
-// function resetTable() {
-//   //alte Tabelle löschen
-//   let table = document.getElementById("myTableBody");
-//   let row = "";
-//   table.innerHTML = row;
-// }
 function buildtable(data) {
 
   //alte Tabelle löschen
@@ -23,31 +17,24 @@ function buildtable(data) {
   }
 }
 function sortieren(n) {
-  let table,
-    rows,
-    switching,
-    i,
-    x,
-    y,
-    shouldSwitch,
-    dir,
+  let table, rows, switching, i, x, y, shouldSwitch, dir,
     switchcount = 0;
+//Tabelle finden
   table = document.getElementById("tabelleEmission");
   switching = true;
-
+//Erste Sortierrichtung setzen
   dir = "asc";
-
+//While schleife die läuft bis nichts mehr sortiert wird  
   while (switching) {
-    switching = false;
+    switching = false; //es wurde nichts sortiert
     rows = table.rows;
-
-    for (i = 1; i < rows.length - 1; i++) {
+//-1 da die erste Zeile die Überschriften beinhaltet und nicht relevant ist!
+    for (i = 1; i < rows.length - 1; i++) { 
       shouldSwitch = false;
-
+//Auswahl der zuvergleichenden Objekte X=aktuelle Reihe, Y aus der nächsten Reihe
       x = rows[i].getElementsByTagName("td")[n];
-      console.log(x);
       y = rows[i + 1].getElementsByTagName("td")[n];
-
+//Vergleich der 2 Zeilen und bestimmen ob sortiert werden muss aufsteigend x > Y, absteigend x < y 
       if (dir == "asc") {
         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
           shouldSwitch = true;
@@ -60,11 +47,12 @@ function sortieren(n) {
         }
       }
     }
+//Auswertung und ggfs. sortieren     
     if (shouldSwitch) {
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
-
       switchcount++;
+//wenn nichtsortiertwird und es ist aufsteigend wechseln auf absteigendesortierung       
     } else {
       if (switchcount == 0 && dir == "asc") {
         dir = "desc";
@@ -73,32 +61,29 @@ function sortieren(n) {
     }
   }
 }
-
-
-
 function suchen() {
+//Definieren der benötigten Variablen   
   let input, filter, table, tr, tdNull, tdEins, i, txtValueNull, txtValueEins;
-  input = document.getElementById("tbInputFilter");
-  filter = input.value.toLowerCase();
-  table = document.getElementById("tabelleEmission");
-  tr = table.getElementsByTagName("tr");
-console.log(input)
-  
-
+  input = document.getElementById("tbInputFilter"); //Finden des Eingabefeldes auf der Seite 
+  filter = input.value.toLowerCase(); //Alle Eingaben als kleinbuchstaben damit Groß- und Kleinschreibung beim filtern egal ist
+  table = document.getElementById("tabelleEmission"); //bestimmen welche Tabelle gefiltert werden soll
+  tr = table.getElementsByTagName("tr"); //Eeihen sind alle Elemente der Tabelle mit dem namen "tr"
+  // Mit einer for Schleife wird der Filter auf jede Zeile angewandt, so oft bis i gleich der anzahl der Reihen ist
   for (let i = 0; i < tr.length; i++) {
-    tdNull = tr[i].getElementsByTagName("td")[0];
-    tdEins = tr[i].getElementsByTagName("td")[1];
+    tdNull = tr[i].getElementsByTagName("td")[0]; // Row 0 = Spalte Land Inhalt der Zeile mit index i
+    tdEins = tr[i].getElementsByTagName("td")[1]; // Row 1 = Spalte Unternehmen Inhalt der Zeile mit index i
+
     if ((tdNull, tdEins)) {
-      txtValueNull = tdNull.textContent || tdNull.innerText;
-      txtValueEins = tdEins.textContent || tdEins.innerText;
+      txtValueNull = tdNull.textContent || tdNull.innerText; //bestimmen des Textinhaltes des Tabellenfeldes Land  
+      txtValueEins = tdEins.textContent || tdEins.innerText; //bestimmen des Textinhaltes des Tabellenfeldes Unternehmen
 
       if (
-        txtValueNull.toLowerCase().indexOf(filter) > -1 ||
-        txtValueEins.toLowerCase().indexOf(filter) > -1
+        txtValueNull.toLowerCase().indexOf(filter) > -1 || //vergleichen des Textinhaltes des Tabellenfeldes Land mit der Eingabe
+        txtValueEins.toLowerCase().indexOf(filter) > -1 //vergleichen des Textinhaltes des Tabellenfeldes Unternehmen mit der Eingabe
       ) {
-        tr[i].style.display = "";
+        tr[i].style.display = ""; //Bei Übereinstimmung wird die Zeile angezeigt
       } else {
-        tr[i].style.display = "None";
+        tr[i].style.display = "None"; //Keine Übereinstümmung, die Zeile wird ausgeblendet
       }
     }
   }
